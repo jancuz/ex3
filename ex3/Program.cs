104ae1a18f3b49ad8b902dfc17eeb44761a31a82
jancuz <jancuz@yandex.ru>
2018-05-31 17:16:50 +0500

    Решена задача №1

--- ex3/Program.cs
+++ ex3/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyLibrary;
 
 namespace ex3
 {
@@ -10,6 +11,12 @@ namespace ex3
     {
         static void Main(string[] args)
         {
+            double x = AskData.ReadDoubleNumber("Введите координату x: ", double.MinValue, double.MaxValue);
+            double y = AskData.ReadDoubleNumber("Введите координату у: ", double.MinValue, double.MaxValue);
+
+            bool ans = (y <= 3 * x + 2 && y <= -3 * x + 2 && y >= -1);
+
+            Console.WriteLine("Ответ: " + ans);
         }
     }
 }
